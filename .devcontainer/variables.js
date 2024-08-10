60098b3458af8df7141224b8935f257e535fa013
souvikdas4242idr <souvikdas4242idrr@gmail.com>
2024-08-10 10:38:09 +0000

    container config done

---
+++ .devcontainer/variables.js
@@ -0,0 +1,8 @@
+const account_ID = 14553
+let account_email = "hitesh@google.com"
+var account_password = "12345"
+accountCity = "Jaipur"
+// account_ID = 2
+account_email = "hc@hc.com"
+console.log(account_ID)
+console.table([account_ID,account_email,account_password,accountCity])
